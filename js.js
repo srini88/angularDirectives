72c1824b57bfa0e3ba29913a7f271f863f092974
srini88 <pvsrinivas.ece@gmail.com>
2016-06-05 14:27:39 -0500

    ng-repeat in div

--- js.js
+++ js.js
@@ -3,13 +3,17 @@ angular.module('app', []);
 angular.module('app').controller('mainCtrl', function($scope){
 	$scope.user = {
 		name :'Srinivas',
-		jobd :"front  end developer"
+		job :"front  end developer",
+		friends :[
+		'viswanath',
+		'palghat'
+		]
 	}
 });
 //creating a directive... give a name ..it returns an object..
 angular.module('app').directive('userInfoCard', function(){
 	return {   
-		template :"Name :{{user.name}} <br/> <div ng-show='user.job'>job: {{user.job}} </div>",
+		template :"Name :{{user.name}} <br/><div ng-show='user.job'>job: {{user.job}} </div>  <div>Friends: <div ng-repeat='friend in user.friends'>{{friend}}</div></div>   ",
 		restrict:"E"  
 	}
 })
@@ -18,3 +22,8 @@ angular.module('app').directive('userInfoCard', function(){
 //our directive can handle html or data from scope object..
 //it can also handle other directives..
 //use ng-show
+
+//we wil do one div for each friend
+
+
+//if I put the next div in the next line..code is getting broken--find out
