0d362b3be4e91b4165c092017122475e4f6e02c8
srini88 <pvsrinivas.ece@gmail.com>
2016-06-05 14:02:09 -0500

    restricting to custom elements only

--- js.js
+++ js.js
@@ -8,6 +8,6 @@ angular.module('app').directive('userInfoCard', function(){
 	return {   //returning obj
 		//template is the attribbute -- as a string--this text is displayed..
 		template :"User Info Card",
-		restrict:"A"  //NOW CHANGED VIEW TO ATTRIBUTE 
+		restrict:"E"  //ONLY CUSTOM ELEMENTS
 	}
 })
