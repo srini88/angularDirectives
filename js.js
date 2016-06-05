e182e9f502b50b2778fc746afd780487c3485ffd
srini88 <pvsrinivas.ece@gmail.com>
2016-06-05 16:16:48 -0500

    different child scope ..but still can access parent

--- js.js
+++ js.js
@@ -9,20 +9,19 @@ angular.module('app').controller('mainCtrl', function($scope){
 		'palghat'
 		]
 	}
-	console.log($scope);
+	console.log($scope)
 });
 
 angular.module('app').directive('userInfoCard', function(){
 	return {   
 		templateUrl :"userInfo.html",
 		restrict:"E",
+		scope:true,
 		controller :function($scope){
 			$scope.knightMe = function(user){
-				console.log("clicked");
-				console.log(user)  //Object {name: "Srinivas", job: "front  end developer", friends: Array[2]}
 				user.rank = "jesus";
 			}
-			console.log($scope);
+			console.log($scope)
 		}
 	}
 })
@@ -36,3 +35,19 @@ angular.module('app').directive('userInfoCard', function(){
 ///now knight me is served from the directive itself...
 
 // above scope in the mainCtrl and in the controler inside userInfoCard is same..
+
+
+//before inherited scope -- - i expanded the console. log from both $scopes ...both have same id...
+
+
+// create a directive with inherited scope...
+
+///after createing shared scope , with scoep:true/// the ids' are diffferent in console.logs
+
+//child scope has a parent property........which is themainCtrl..
+///still the child scope in directive can access the parent scope user...(that's not the point)
+//by setting scope true -- we can add new shit to child - that cannot be seen by the parent..
+
+
+
+///Directive with isolated scope..
