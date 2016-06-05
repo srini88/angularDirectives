069766db17da9da3b0f513ba98b6c5a6ffcfade5
srini88 <pvsrinivas.ece@gmail.com>
2016-06-05 13:55:53 -0500

    userInfoCard 1st way..not right .better custom ele

---
+++ js.js
@@ -0,0 +1,12 @@
+angular.module('app', []);
+
+angular.module('app').controller('mainCtrl', function($scope){
+
+});
+//creating a directive... give a name ..it returns an object..
+angular.module('app').directive('userInfoCard', function(){
+	return {   //returning obj
+		//template is the attribbute -- as a string--this text is displayed..
+		template :"User Info Card"
+	}
+})
---
+++ notes.js
@@ -0,0 +1,36 @@
+
+your first directive
+
+events, scope and controller
+
+decorator directives 
+
+transclusion...
+
+
+
+structureal directives -- modifies structure of dom 
+
+controllers and nesteed directives 
+
+directives and services...
+
+4 different ways to specify directives...
+
+one way is to use attribute..
+2nd way brand new custom tag ..<my-directive> </my-directive>
+
+other ways not recommended....
+
+
+1st way --- implies modifying existing element --
+2nd way - implies adding new object to teh page..
+
+
+userInfoCard directive ...changes to user-info-card in the view...remember..have to use dashes...coz html is not case -sensitive..  dasherization or snakecasing..
+
+also use colons user:info:card 
+also underscores user_info_card 
+
+also prefix with  x, or data...
+
