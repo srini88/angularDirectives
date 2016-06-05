0da55c8671401b32c2dcade97083eb7388828086
srini88 <pvsrinivas.ece@gmail.com>
2016-06-05 14:05:37 -0500

    added dynamic bindings

--- js.js
+++ js.js
@@ -1,13 +1,15 @@
 angular.module('app', []);
 
 angular.module('app').controller('mainCtrl', function($scope){
-
+	$scope.user = {
+		name :'Srinivas',
+		job :"front  end developer"
+	}
 });
 //creating a directive... give a name ..it returns an object..
 angular.module('app').directive('userInfoCard', function(){
-	return {   //returning obj
-		//template is the attribbute -- as a string--this text is displayed..
-		template :"User Info Card",
+	return {   
+		template :"Name :{{user.name}} <br/> job: {{user.job}}",
 		restrict:"E"  //ONLY CUSTOM ELEMENTS
 	}
 })
