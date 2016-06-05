f7285fdb714a45561723a8634ed788b6ba307f3b
srini88 <pvsrinivas.ece@gmail.com>
2016-06-05 14:00:06 -0500

    restrict A is attribute

--- js.js
+++ js.js
@@ -7,6 +7,7 @@ angular.module('app').controller('mainCtrl', function($scope){
 angular.module('app').directive('userInfoCard', function(){
 	return {   //returning obj
 		//template is the attribbute -- as a string--this text is displayed..
-		template :"User Info Card"
+		template :"User Info Card",
+		restrict:"A"  //with cap A -- the contnet will disappeer...coz we using element in the view.. 
 	}
 })
