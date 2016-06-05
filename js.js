b475877621e6f96af8009b6131ceda0e6104f670
srini88 <pvsrinivas.ece@gmail.com>
2016-06-05 14:23:40 -0500

    ng-show will only show if user has job prop

--- js.js
+++ js.js
@@ -3,13 +3,18 @@ angular.module('app', []);
 angular.module('app').controller('mainCtrl', function($scope){
 	$scope.user = {
 		name :'Srinivas',
-		job :"front  end developer"
+		jobd :"front  end developer"
 	}
 });
 //creating a directive... give a name ..it returns an object..
 angular.module('app').directive('userInfoCard', function(){
 	return {   
-		template :"Name :{{user.name}} <br/> job: {{user.job}}",
-		restrict:"E"  //ONLY CUSTOM ELEMENTS
+		template :"Name :{{user.name}} <br/> <div ng-show='user.job'>job: {{user.job}} </div>",
+		restrict:"E"  
 	}
 })
+
+
+//our directive can handle html or data from scope object..
+//it can also handle other directives..
+//use ng-show
